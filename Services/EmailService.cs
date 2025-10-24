cf14e1c29b7a0c7fe4f11330815428676798405a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-24 12:02:58 -0600

    Servicio para enviar correo

---
+++ Services/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using NecesidadesCapacitacion.Data;
+using NecesidadesCapacitacion.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+
+namespace NecesidadesCapacitacion.Services
+{
+    public class EmailService
+    {
+        public EmailSettings Setting { get; }
+
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            Setting = options.Value;
+        }
+
+        public async Task SendEmailAsync(IEnumerable<string> toEmails, string subject, string body)
+        {
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(Setting.SenderEmail, Setting.SenderName),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+                Priority = MailPriority.High,
+            };
+
+            foreach (var email in toEmails)
+            {
+                if (!string.IsNullOrWhiteSpace(email))
+                {
+                    mailMessage.To.Add(email);
+                }
+            }
+
+            using var client = new SmtpClient
+            {
+                Host = Setting.Host,
+                Port = Setting.Port,
+                EnableSsl = Setting.UseSSL,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(Setting.Username, Setting.Password)
+            };
+
+            await client.SendMailAsync(mailMessage);
+        }        
+    }
+}
