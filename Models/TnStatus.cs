d0310b1ff6898adc06a688a2f63649c30077c8f0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-27 07:42:36 -0600

    Modelo Status

---
+++ Models/TnStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NecesidadesCapacitacion.Models;
+
+public partial class TnStatus
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public virtual ICollection<TrainingNeeds> TrainingNeeds { get; set; } = new List<TrainingNeeds>();
+}
